f26f9dfb57ee622d598712a4f2c8ab12c0208f02
Mahmoud Ahmed <mahmoudahmed18840@gmail.com>
2025-04-28 03:35:14 +0300

    create crud operation for admin

--- BusinessLogic/Service/IUserService.cs
+++ BusinessLogic/Service/IUserService.cs
@@ -1,5 +1,7 @@
 ﻿using DataAccess.Models;
+using Furni_Ecommerce_Shared.AdminViewModel;
 using Furni_Ecommerce_Shared.UserViewModel;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +13,11 @@ namespace BusinessLogic.Service
     public interface IUserService
     {
         public ApplicationUser Register(RegisterViewModel model);
+        public Task< List<AllAdminViewModel>> GetAllAdmin();
+
+        public ApplicationUser CreateAdmin(AdminFormViewModel model);
+        public Task<EditAdminViewModel> GetSpecificAdmin(string adminId);
+        public Task<List<RoleSelectionViewModel>> GetAvailableRoles();
+        public Task<IdentityResult> EditAdmin(EditAdminViewModel model);
     }
 }

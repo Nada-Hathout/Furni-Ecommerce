44ecad1bc85533569ff6df70eb92381c92740d9f
FatimaOElfar <fatma.2002nagy@gmail.com>
2025-05-10 21:55:22 +0300

    forget-password

---
+++ Furni_Ecommerce_Shared/UserViewModel/ٌResetPasswordViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Furni_Ecommerce_Shared.UserViewModel
+{
+  public  class ResetPasswordViewModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+        public string Token { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
